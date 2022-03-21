e9aff258f3166454949e3965c8c173ee8156fbbb
GulievCorp <guliev.anar1998@gmail.com>
2022-03-21 22:07:13 +0300

    add files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        main: './src/index.js',
+        alatytics: './src/analytics.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     'mode': 'development'
