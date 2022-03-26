0b7da01b7fde5d12af2a2ca5397308d1c56cb970
GulievCorp <guliev.anar1998@gmail.com>
2022-03-26 14:15:16 +0300

    1.13.06 finished file loader and fonts

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import Post from "./Post";
 import json from '../assets/json.json';
 import './styles/styles.css';
+import Pepe from '../assets/pepe.jpg'
+
+let pepeImg = document.querySelector('.pepe');
+
+pepeImg.src = Pepe;
+pepeImg.alt = 'PEPE'
 
 console.log(json);
 
-const post = new Post('Webpack Post Title');
+const post = new Post('Webpack Post Title', Pepe);
 
 console.log('Post to string:', post.toString());
